26c5057d56789d9a4437dcd42f85fdb8f3db5ef6
Ahmed Rizwan <riz.ahmed@smarterbuild.net>
2025-10-09 01:48:54 +0900

    teachertype required false

--- src/domains/Users/schema.js
+++ src/domains/Users/schema.js
@@ -10,7 +10,7 @@ const  UserSchema = new mongoose.Schema({
   username:{ type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "student","teacher","parent"], default: "" ,required :true},
-  teacherType : { type: String, enum: ["Home", "Native","PartTime"], default: "" },
+  teacherType : { type: String, enum: ["Home", "Native","PartTime"], default: "",  required: false },
   driverName :{type: String},
   driverAge :{type: String},
   driverArcNumber :{type: String},
